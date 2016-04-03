41ea71f3734571845972b8ef989ea305c729f541
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-04-03 21:16:03 +0200

    Add an exception per service

--- iMobileDevice.Generator/NameConversions.cs
+++ iMobileDevice.Generator/NameConversions.cs
@@ -4,40 +4,107 @@
 
 namespace iMobileDevice.Generator
 {
+    using System;
+    using System.Collections.Generic;
     using System.Text;
 
     internal static class NameConversions
     {
         public static string ToClrName(string nativeName, NameConversion conversion)
         {
-            string[] parts = nativeName.Split('_');
+            List<string> parts = new List<string>(nativeName.Split('_'));
 
             StringBuilder nameBuilder = new StringBuilder();
 
-            for (int i = 0; i < parts.Length; i++)
+            int i = 0;
+
+            while (i < parts.Count)
             {
                 if (
                     (conversion == NameConversion.Function || conversion == NameConversion.Field)
                     && i == 0)
                 {
+                    i++;
                     continue;
                 }
 
                 if (i == 1 && parts[i] == "E")
                 {
+                    i++;
                     continue;
                 }
 
                 if (parts[i] == "t")
                 {
+                    i++;
                     continue;
+                }
+
+                // Manual patching of naming inconsistencies
+                if (parts[i] == "instproxy")
+                {
+                    parts[i] = "Installation";
+                    parts.Insert(i + 1, "Proxy");
+                }
+                else if (parts[i] == "lockdownd")
+                {
+                    parts[i] = "lockdown";
+                }
+                else if (parts[i] == "np")
+                {
+                    parts[i] = "Notification";
+                    parts.Insert(i + 1, "Proxy");
+                }
+                else if (parts[i] == "restored")
+                {
+                    parts[i] = "restore";
+                }
+                else if (parts[i] == "debugserver")
+                {
+                    parts[i] = "Debug";
+                    parts.Insert(i + 1, "Server");
+                }
+                else if (parts[i] == "libimobiledevice")
+                {
+                    parts[i] = "iDevice";
+                }
+                else if (parts[i] == "heartbeat")
+                {
+                    parts[i] = "Heart";
+                    parts.Insert(i + 1, "Beat");
+                }
+                else if (parts[i] == "mobilebackup")
+                {
+                    parts[i] = "Mobile";
+                    parts.Insert(i + 1, "Backup");
+                }
+                else if (parts[i] == "mobilebackup2")
+                {
+                    parts[i] = "Mobile";
+                    parts.Insert(i + 1, "Backup2");
+                }
+                else if (parts[i] == "mobilesync")
+                {
+                    parts[i] = "Mobile";
+                    parts.Insert(i + 1, "Sync");
+                }
+                else if (parts[i] == "webinspector")
+                {
+                    parts[i] = "Web";
+                    parts.Insert(i + 1, "Inspector");
+                }
+                else if (parts[i] == "sbservices")
+                {
+                    parts[i] = "Spring";
+                    parts.Insert(i + 1, "Board");
+                    parts.Insert(i + 2, "Services");
                 }
 
                 if (conversion == NameConversion.Parameter && i == 0)
                 {
                     nameBuilder.Append(char.ToLowerInvariant(parts[i][0]) + parts[i].Substring(1).ToLowerInvariant());
                 }
-                else if (parts[i].StartsWith("idevice"))
+                else if (parts[i].StartsWith("idevice", StringComparison.OrdinalIgnoreCase))
                 {
                     nameBuilder.Append("i" + char.ToUpperInvariant(parts[i][1]) + parts[i].Substring(2).ToLowerInvariant());
                 }
@@ -45,6 +112,8 @@ namespace iMobileDevice.Generator
                 {
                     nameBuilder.Append(char.ToUpperInvariant(parts[i][0]) + parts[i].Substring(1).ToLowerInvariant());
                 }
+
+                i++;
             }
 
             return nameBuilder.ToString();
