80322750cd97051b0b33e6f2ea2696c7d654e925
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-04-04 22:32:25 +0200

    Get all libimobiledevice libraries from NuGet

--- iMobileDevice.Generator/Program.cs
+++ iMobileDevice.Generator/Program.cs
@@ -15,18 +15,25 @@ namespace iMobileDevice.Generator
             var sourceDirectory = args[0];
             var targetDirectory = args[1];
 
+            sourceDirectory = Path.GetFullPath(sourceDirectory);
+            targetDirectory = Path.GetFullPath(targetDirectory);
+
+            Console.WriteLine($"Reading libimobiledevice headers from: {sourceDirectory}");
+            Console.WriteLine($"Writing the C# files to: {targetDirectory}");
+
             ModuleGenerator generator = new ModuleGenerator();
 
             generator.IncludeDirectories.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Microsoft Visual Studio 14.0\VC\include"));
             generator.IncludeDirectories.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Windows Kits\10\Include\10.0.10240.0\ucrt\"));
-            generator.IncludeDirectories.Add(Path.Combine(sourceDirectory, @"libimobiledevice\include"));
-            generator.IncludeDirectories.Add(Path.Combine(sourceDirectory, @"libplist\include"));
-            generator.IncludeDirectories.Add(Path.Combine(sourceDirectory, @"libusbmuxd\include"));
+            generator.IncludeDirectories.Add(Path.Combine(sourceDirectory, @"packages\libimobiledevice.1.2.0\build\native\include\"));
+            generator.IncludeDirectories.Add(Path.Combine(sourceDirectory, @"packages\libplist.1.12.47\build\native\include"));
+            generator.IncludeDirectories.Add(Path.Combine(sourceDirectory, @"packages\libusbmuxd.1.0.10.13\build\native\include"));
 
             Collection<string> names = new Collection<string>();
 
-            foreach (var file in Directory.GetFiles(Path.Combine(sourceDirectory, @"libimobiledevice\include\libimobiledevice"), "*.h"))
+            foreach (var file in Directory.GetFiles(Path.Combine(sourceDirectory, @"packages\libimobiledevice.1.2.0\build\native\include\libimobiledevice"), "*.h"))
             {
+                Console.WriteLine($"Processing {Path.GetFileName(file)}");
                 generator.InputFile = file;
                 generator.Generate(targetDirectory);
                 generator.Types.Clear();
