b26222d244f6f61871cba641123f43cb6b3a3797
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-04-05 19:55:40 +0200

    Update libimobiledevice and libplist dependencies.

--- iMobileDevice.Generator/Program.cs
+++ iMobileDevice.Generator/Program.cs
@@ -26,15 +26,15 @@ namespace iMobileDevice.Generator
 
             generator.IncludeDirectories.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Microsoft Visual Studio 14.0\VC\include"));
             generator.IncludeDirectories.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Windows Kits\10\Include\10.0.10240.0\ucrt\"));
-            generator.IncludeDirectories.Add(Path.Combine(sourceDirectory, @"packages\libimobiledevice.1.2.0.20\build\native\include\"));
-            generator.IncludeDirectories.Add(Path.Combine(sourceDirectory, @"packages\libplist.1.12.47\build\native\include"));
+            generator.IncludeDirectories.Add(Path.Combine(sourceDirectory, @"packages\libimobiledevice.1.2.0.21\build\native\include\"));
+            generator.IncludeDirectories.Add(Path.Combine(sourceDirectory, @"packages\libplist.1.12.48\build\native\include"));
             generator.IncludeDirectories.Add(Path.Combine(sourceDirectory, @"packages\libusbmuxd.1.0.10.13\build\native\include"));
 
             Collection<string> names = new Collection<string>();
 
             var files = new List<string>();
-            files.AddRange(Directory.GetFiles(Path.Combine(sourceDirectory, @"packages\libimobiledevice.1.2.0.20\build\native\include\libimobiledevice"), "*.h"));
-            files.Add(Path.Combine(sourceDirectory, @"packages\libplist.1.12.47\build\native\include\plist\plist.h"));
+            files.AddRange(Directory.GetFiles(Path.Combine(sourceDirectory, @"packages\libimobiledevice.1.2.0.21\build\native\include\libimobiledevice"), "*.h"));
+            files.Add(Path.Combine(sourceDirectory, @"packages\libplist.1.12.48\build\native\include\plist\plist.h"));
 
             foreach (var file in files)
             {
