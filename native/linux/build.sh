c8d8c9f187664723b113b419853e3f88ae9d3c8a
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-06-23 15:13:42 -0700

    Include clean in the build script, add mini-README file

--- native/linux/build.sh
+++ native/linux/build.sh
@@ -6,23 +6,30 @@ out=$path/out/usr/local
 export PKG_CONFIG_PATH=$out/lib/pkgconfig/
 echo PKG_CONFIG_PATH set to $PKG_CONFIG_PATH
 
+# Remove the output folder, if it already exists
+rm -rf $out
+
 cd $path/libplist
 ./autogen.sh --prefix=$out --without-cython
+make clean
 make
 make install
 
 cd $path/libusbmuxd
 ./autogen.sh --prefix=$out --without-cython
+make clean
 make
 make install
 
 cd $path/libimobiledevice
 ./autogen.sh --prefix=$out --without-cython
+make clean
 make
 make install
 
 cd $path/usbmuxd
 ./autogen.sh --prefix=$out --with-udevrulesdir=$out/lib/udev/rules.d
+make clean
 make
 make install
 
