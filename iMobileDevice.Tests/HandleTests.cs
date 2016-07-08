e0eeba8235b0119b82877ed1a02934a7cfddb8d5
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-07-08 14:41:53 +0200

    Add unit test for the Handle class

---
+++ iMobileDevice.Tests/HandleTests.cs
@@ -0,0 +1,37 @@
+﻿namespace iMobileDevice.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Afc;
+
+    [TestClass]
+    public class HandleTests
+    {
+        // Because handles are generate, we only test one class of handles.
+
+        [TestMethod]
+        public void ZeroTest()
+        {
+            var handle = AfcClientHandle.Zero;
+            Assert.IsTrue(handle.IsInvalid);
+            Assert.IsFalse(handle.IsClosed);
+        }
+
+        [TestMethod]
+        public void EqualsTest()
+        {
+            AfcClientHandle handle = AfcClientHandle.DangerousCreate(new IntPtr(42));
+            AfcClientHandle handle2 = AfcClientHandle.DangerousCreate(new IntPtr(42));
+            AfcClientHandle zero = AfcClientHandle.Zero;
+
+            Assert.IsTrue(handle.Equals(handle));
+            Assert.IsTrue(handle.Equals(handle2));
+            Assert.IsTrue(handle2.Equals(handle));
+
+            Assert.IsFalse(handle.Equals(zero));
+            Assert.IsFalse(zero.Equals(handle));
+
+            Assert.IsFalse(handle.Equals(42));
+        }
+    }
+}
