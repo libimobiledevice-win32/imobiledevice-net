ef3882afbcb3a8001d3423bda7455f1476ff7976
Frederik Carlier <frederik.carlier@quamotion.mobi>
2016-09-03 01:30:25 +0200

    Remove .NET dependencies from list of .so files we copy

--- native/runtime.linux-x64.iMobileDevice-net/build.sh
+++ native/runtime.linux-x64.iMobileDevice-net/build.sh
@@ -29,10 +29,32 @@ make
 make install
 
 cd $path/usbmuxd
-./autogen.sh --prefix=$out --with-udevrulesdir=$out/lib/udev/rules.d
+./autogen.sh --prefix=$out --with-udevrulesdir=$out/lib/udev/rules.d --without-systemd
 make clean
 make
 make install
 
 cd $path
 
+cp /usr/lib/x86_64-linux-gnu/libusb-1.0.so $out/lib/
+
+# Patch the so dependencies for all executables in the out directory 
+# Redirect from libimobiledevice.so.6 to libimobiledevice.so,
+# and change the out folder in the rpath to ${ORIGIN}
+# (this will be the setup in our target environment)
+#
+# For more info, see:
+# https://github.com/NixOS/patchelf
+# http://man7.org/linux/man-pages/man8/ld.so.8.html
+
+patchelf=patchelf-0.9/src/patchelf
+
+for f in $out/*bin/*; do
+   chmod +w $f
+
+   $patchelf --set-rpath '${ORIGIN}' $f
+   $patchelf --remove-needed libimobiledevice.so.6 $f
+   $patchelf --add-needed libimobiledevice.so $f
+
+   # readelf -d $f
+done
