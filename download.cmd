21fd9472250a513a02ebd2f5edcd612bf2c71069
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-10-07 20:36:24 +0200

    Bump libimobiledevice dependency, drop ARM/ARM64 for now

--- download.cmd
+++ download.cmd
@@ -1,30 +1,30 @@
-set USBMUXD_VERSION=95
+set USBMUXD_VERSION=127
 set LIBIMOBILEDEVICE_VERSION=118
 wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/usbmuxd-osx-x64-1.1.0-%USBMUXD_VERSION%.tar.gz -O ext\usbmuxd-osx-x64-1.1.0-%USBMUXD_VERSION%.tar.gz
-wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/usbmuxd-linux-arm-1.1.0-%USBMUXD_VERSION%.tar.gz -O ext\usbmuxd-linux-arm-1.1.0-%USBMUXD_VERSION%.tar.gz
-wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/usbmuxd-linux-arm64-1.1.0-%USBMUXD_VERSION%.tar.gz -O ext\usbmuxd-linux-arm64-1.1.0-%USBMUXD_VERSION%.tar.gz
+REM wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/usbmuxd-linux-arm-1.1.0-%USBMUXD_VERSION%.tar.gz -O ext\usbmuxd-linux-arm-1.1.0-%USBMUXD_VERSION%.tar.gz
+REM wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/usbmuxd-linux-arm64-1.1.0-%USBMUXD_VERSION%.tar.gz -O ext\usbmuxd-linux-arm64-1.1.0-%USBMUXD_VERSION%.tar.gz
 wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/usbmuxd-linux-x64-1.1.0-%USBMUXD_VERSION%.tar.gz -O ext\usbmuxd-linux-x64-1.1.0-%USBMUXD_VERSION%.tar.gz
 wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/libimobiledevice-osx-x64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -O ext\libimobiledevice-osx-x64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz
-wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/libimobiledevice-linux-arm64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -O ext\libimobiledevice-linux-arm64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz
-wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/libimobiledevice-linux-arm-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -O ext\libimobiledevice-linux-arm-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz
+REM wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/libimobiledevice-linux-arm64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -O ext\libimobiledevice-linux-arm64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz
+REM wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/libimobiledevice-linux-arm-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -O ext\libimobiledevice-linux-arm-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz
 wget -nc https://qmcdn.blob.core.windows.net/imobiledevice/libimobiledevice-linux-x64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -O ext\libimobiledevice-linux-x64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz
 
 REM extract tar file from tar.gz
 7z x -aos ext\usbmuxd-osx-x64-1.1.0-%USBMUXD_VERSION%.tar.gz -oext\
-7z x -aos ext\usbmuxd-linux-arm-1.1.0-%USBMUXD_VERSION%.tar.gz -oext\
-7z x -aos ext\usbmuxd-linux-arm64-1.1.0-%USBMUXD_VERSION%.tar.gz -oext\
+REM 7z x -aos ext\usbmuxd-linux-arm-1.1.0-%USBMUXD_VERSION%.tar.gz -oext\
+REM 7z x -aos ext\usbmuxd-linux-arm64-1.1.0-%USBMUXD_VERSION%.tar.gz -oext\
 7z x -aos ext\usbmuxd-linux-x64-1.1.0-%USBMUXD_VERSION%.tar.gz -oext\
 7z x -aos ext\libimobiledevice-osx-x64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -oext\
-7z x -aos ext\libimobiledevice-linux-arm64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -oext\
-7z x -aos ext\libimobiledevice-linux-arm-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -oext\
+REM 7z x -aos ext\libimobiledevice-linux-arm64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -oext\
+REM 7z x -aos ext\libimobiledevice-linux-arm-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -oext\
 7z x -aos ext\libimobiledevice-linux-x64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar.gz -oext\
 
 REM extract files from the tar files
 7z x -aos ext\usbmuxd-osx-x64-1.1.0-%USBMUXD_VERSION%.tar -oext\osx-64
-7z x -aos ext\usbmuxd-linux-arm-1.1.0-%USBMUXD_VERSION%.tar -oext\debian-arm
-7z x -aos ext\usbmuxd-linux-arm64-1.1.0-%USBMUXD_VERSION%.tar -oext\debian-arm64
+REM 7z x -aos ext\usbmuxd-linux-arm-1.1.0-%USBMUXD_VERSION%.tar -oext\debian-arm
+REM 7z x -aos ext\usbmuxd-linux-arm64-1.1.0-%USBMUXD_VERSION%.tar -oext\debian-arm64
 7z x -aos ext\usbmuxd-linux-x64-1.1.0-%USBMUXD_VERSION%.tar -oext\debian-x64
 7z x -aos ext\libimobiledevice-osx-x64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar -oext\osx-x64
-7z x -aos ext\libimobiledevice-linux-arm64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar -oext\debian-arm64
-7z x -aos ext\libimobiledevice-linux-arm-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar -oext\debian-arm
+REM 7z x -aos ext\libimobiledevice-linux-arm64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar -oext\debian-arm64
+REM 7z x -aos ext\libimobiledevice-linux-arm-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar -oext\debian-arm
 7z x -aos ext\libimobiledevice-linux-x64-1.2.1-%LIBIMOBILEDEVICE_VERSION%.tar -oext\debian-x64
