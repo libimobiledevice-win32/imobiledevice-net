0c42e3a0928a19aebfdeec7d31d86000715d8def
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-05-15 17:44:18 +0200

    Use vcpkg

--- iMobileDevice.Generator/Program.cs
+++ iMobileDevice.Generator/Program.cs
@@ -51,33 +51,25 @@ namespace iMobileDevice.Generator
             Console.WriteLine($"Reading libimobiledevice headers from: {sourceDirectory}");
             Console.WriteLine($"Writing the C# files to: {targetDirectory}");
 
-            var nativePropsPath = Path.Combine(targetDirectory, "../native.props");
-            var nativeProps = XDocument.Load(nativePropsPath);
-            var ideviceinstaller = nativeProps.Element("Project").Element("PropertyGroup").Element("ideviceinstaller").Value;
-            var libimobiledevice = nativeProps.Element("Project").Element("PropertyGroup").Element("libimobiledevice").Value;
-            var libideviceactivation = nativeProps.Element("Project").Element("PropertyGroup").Element("libideviceactivation").Value;
-            var libusbmuxd = nativeProps.Element("Project").Element("PropertyGroup").Element("libusbmuxd").Value;
-            var libplist = nativeProps.Element("Project").Element("PropertyGroup").Element("libplist").Value;
-            var usbmuxd = nativeProps.Element("Project").Element("PropertyGroup").Element("usbmuxd").Value;
+            var vcpkgPath = Environment.GetEnvironmentVariable("VCPKG_ROOT");
+            vcpkgPath = Path.Combine(vcpkgPath, "installed", "x86-windows", "include");
+            Console.WriteLine($"Reading include files from {vcpkgPath}");
 
             ModuleGenerator generator = new ModuleGenerator();
             generator.IncludeDirectories.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Microsoft Visual Studio 14.0\VC\include"));
             generator.IncludeDirectories.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Microsoft Visual Studio\Shared\14.0\VC\include"));
             generator.IncludeDirectories.Add(GetWindowsKitUcrtFolder());
             generator.IncludeDirectories.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), @"Windows Kits", "8.1", "include", "shared"));
-            generator.IncludeDirectories.Add(Path.Combine(packagesDirectory, $@"libusbmuxd\{libusbmuxd}\build\native\include\"));
-            generator.IncludeDirectories.Add(Path.Combine(packagesDirectory, $@"libimobiledevice\{libimobiledevice}\build\native\include\"));
-            generator.IncludeDirectories.Add(Path.Combine(packagesDirectory, $@"libplist\{libplist}\build\native\include"));
-            generator.IncludeDirectories.Add(Path.Combine(packagesDirectory, $@"libideviceactivation\{libideviceactivation}\build\native\include\libideviceactivation"));
+            generator.IncludeDirectories.Add(Path.Combine(vcpkgPath));
 
             Collection<string> names = new Collection<string>();
 
             var files = new List<string>();
-            files.Add(Path.Combine(packagesDirectory, $@"libusbmuxd\{libusbmuxd}\build\native\include\usbmuxd.h"));
-            files.Add(Path.Combine(packagesDirectory, $@"libplist\{libplist}\build\native\include\plist\plist.h"));
-            files.Add(Path.Combine(packagesDirectory, $@"libideviceactivation\{libideviceactivation}\build\native\include\libideviceactivation\libideviceactivation.h"));
+            files.Add(Path.Combine(packagesDirectory, Path.Combine(vcpkgPath, "usbmuxd.h")));
+            files.Add(Path.Combine(packagesDirectory, Path.Combine(vcpkgPath, "plist/plist.h")));
+            files.Add(Path.Combine(packagesDirectory, Path.Combine(vcpkgPath, "libideviceactivation.h")));
 
-            var iMobileDeviceDirectory = Path.Combine(packagesDirectory, $@"libimobiledevice\{libimobiledevice}\build\native\include\libimobiledevice");
+            var iMobileDeviceDirectory = Path.Combine(vcpkgPath, "libimobiledevice");
             files.Add(Path.Combine(iMobileDeviceDirectory, "libimobiledevice.h"));
             files.Add(Path.Combine(iMobileDeviceDirectory, "lockdown.h"));
             files.Add(Path.Combine(iMobileDeviceDirectory, "afc.h"));
