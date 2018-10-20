edb4d6355a293a20312e4e1990686f358e8c0500
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-10-20 23:19:04 +0200

    Use gpg

--- gpg-pass.sh
+++ gpg-pass.sh
@@ -7,7 +7,7 @@ export GPG_TTY=`tty`
 echo Running on $DISTRIB_CODENAME
 
 if [ "$DISTRIB_CODENAME" = "trusty" ]; then
-    cat $PWD_FILE | gpg2 --batch --passphrase-fd 0 --yes $@
+    cat $PWD_FILE | gpg --batch --passphrase-fd 0 --yes $@
 else
-    cat $PWD_FILE | gpg2 --batch --passphrase-fd 0 --pinentry-mode loopback --yes $@
+    cat $PWD_FILE | gpg --batch --passphrase-fd 0 --pinentry-mode loopback --yes $@
 fi
