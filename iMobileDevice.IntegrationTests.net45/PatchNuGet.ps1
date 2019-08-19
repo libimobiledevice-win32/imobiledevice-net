dde137f2c38c193167000fa313f515602130fae9
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-08-19 12:38:38 +0200

    Simplify build process - Read NuGet package version from Directory.Build.props - Don't download 7zip as it is already installed - Don't download NuGet using chocolatey (takes up to 5 minutes), but use a pre-defined task

--- iMobileDevice.IntegrationTests.net45/PatchNuGet.ps1
+++ iMobileDevice.IntegrationTests.net45/PatchNuGet.ps1
@@ -1,11 +1,13 @@
-$version = (& $env:USERPROFILE/.nuget/packages/nerdbank.gitversioning/2.1.65/tools/Get-Version.ps1);
+$buildPropsPath = $env:SYSTEM_ARTIFACTSDIRECTORY + "\imobiledevice-net\Directory.Build.props"
+
+[xml]$buildProps = Get-Content $buildPropsPath
+$version = $buildProps.Project.PropertyGroup.MobileDeviceDotNetNuGetVersion
+Write-Host "Current NuGet package version: $version"
 
 $projectFile = Get-Content "iMobileDevice.IntegrationTests.net45.csproj"
-$projectFile = $projectFile.Replace("{imobiledevice-version}", $version.NuGetPackageVersion)
+$projectFile = $projectFile.Replace("{imobiledevice-version}", $version)
 Set-Content "iMobileDevice.IntegrationTests.net45.csproj" $projectFile
-Get-Content "iMobileDevice.IntegrationTests.net45.csproj"
 
 $packagesFile = Get-Content "packages.config"
-$packagesFile = $packagesFile.Replace("{imobiledevice-version}", $version.NuGetPackageVersion)
+$packagesFile = $packagesFile.Replace("{imobiledevice-version}", $version)
 Set-Content "packages.config" $packagesFile
-Get-Content "packages.config"
